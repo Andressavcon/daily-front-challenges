4720c4113ccdb231b7796db612fb7a92294cf3c1
Andressavcon <andressavconrado@gmail.com>
2024-05-22 11:38:15 -0300

    feat: att files

--- blow-up-ballon/script.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector(".container");
-const ballon = document.querySelector(".ballon");
-
-const blowBallon = () => {
-  container.removeChild(ballon);
-};
-
-ballon.addEventListener("click", blowBallon);
